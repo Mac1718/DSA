13b8708f350953e8fca8c76e8bf472a78c482134
Mahant <146162280+Mac1718@users.noreply.github.com>
2024-08-13 20:59:40 +0530

    Add files via upload

---
+++ Basic.java
@@ -0,0 +1,21 @@
+package Recursions;
+
+public class Basic 
+{
+    public static void main(String[] args) 
+    {
+        print(1);
+       
+    }
+    static void print(int n) 
+    {
+        if(n==5)
+        {
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+        print(n+1);
+    }    
+   
+}
