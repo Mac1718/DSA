e793a577282fb512e2c0e63e764864934a75560c
Mahant <146162280+Mac1718@users.noreply.github.com>
2024-08-21 19:29:46 +0530

    Add files via upload

---
+++ FiboinRec.java
@@ -0,0 +1,18 @@
+package Recursions;
+
+public class FiboinRec 
+{
+ public static void main(String[] args) {
+    int ans=fibo(8);
+    System.out.println(ans);
+ }
+
+ static int fibo(int n)
+ {
+    if(n<2)
+    {
+        return n;
+    }
+    return fibo(n-1) + fibo(n-2);
+ }
+}
